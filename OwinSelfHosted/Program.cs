313554ed390491b2955fd2a93063d1df93977dee
Ben Kauffman <ben.kauffman@developmentnow.com>
2016-08-19 15:40:13 -0700

    initial commit for the project

---
+++ OwinSelfHosted/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Net.Http;
+using System.Threading;
+
+namespace OwinSelfHosted
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string baseAddress = "http://localhost:9000/";
+            WebApp.Start<Startup>(url: baseAddress);
+            var spin = new ConsoleSpinner();
+            Console.Write("RESTFUL OWIN is running");
+            while (true)
+            {
+                spin.Turn();
+            }
+
+            //// Start OWIN host 
+            //using (WebApp.Start<Startup>(url: baseAddress))
+            //{
+            //    // Create HttpCient and make a request to api/values 
+            //    HttpClient client = new HttpClient();
+
+            //    var response = client.GetAsync(baseAddress + "api/example").Result;
+
+            //    Console.WriteLine(response);
+            //    Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            //}
+
+            Console.ReadLine();
+        }
+    }
+
+    public class ConsoleSpinner
+    {
+        int counter;
+
+        public void Turn()
+        {
+            counter++;
+            switch (counter % 4)
+            {
+                case 0: Console.Write("/"); counter = 0; break;
+                case 1: Console.Write("-"); break;
+                case 2: Console.Write("\\"); break;
+                case 3: Console.Write("|"); break;
+            }
+            Thread.Sleep(100);
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+        }
+    }
+}
---
+++ OwinSelfHosted/controllers/ExampleController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace OwinSelfHosted
+{
+    [RoutePrefix("example")]
+    public class ExampleController : ApiController
+    {
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            List<string> strList = new List<string>();
+            for(int i = 1; i <= 10; i++)
+            {
+                strList.Add("example value " + i);
+            }
+
+            return strList;
+        }
+
+        // GET api/values/5 
+        [Route("{id:int}")]
+        public string Get(int id)
+        {
+            return id.ToString();
+        }
+
+        // POST api/values 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+        }
+    }
+}
