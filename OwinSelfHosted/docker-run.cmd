411d0724454f791029341691ddc361e29497b79a
Ben Kauffman <ben.kauffman@developmentnow.com>
2016-08-20 12:45:32 -0700

    updated with some really cool run commands, everything is working and running as expected

--- OwinSelfHosted/docker-run.cmd
+++ OwinSelfHosted/docker-run.cmd
@@ -1,8 +1,11 @@
 REM stop all running containers
 docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q) -f
 
 REM run the webservice inside a container
-docker run -p 8080:9000 owin-web-service
+REM docker run -p 8080:9000 owin-web-service
 
 REM attach to the container's bash shell
-docker run -it -p 8080:9000 owin-web-service /bin/bash
+REM docker run -it -p 8080:9000 owin-web-service /bin/bash
+
+docker run -d -h localhost -p 9000:9000 owin-web-service
