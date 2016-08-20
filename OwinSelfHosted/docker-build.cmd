c585555e1a5b182232192b94353e4e406b259864
Ben Kauffman <ben.kauffman@developmentnow.com>
2016-08-20 12:23:03 -0700

    updated docker file, gettin compiler errors due to framework v4.5.2 unavailable

--- OwinSelfHosted/docker-build.cmd
+++ OwinSelfHosted/docker-build.cmd
@@ -1,2 +1,1 @@
-docker build -t mono_4.4.2.11 https://github.com/mono/docker.git#master:4.4.2.11
 docker build -t owin-web-service .
--- OwinSelfHosted/docker-run.cmd
+++ OwinSelfHosted/docker-run.cmd
@@ -1,1 +1,8 @@
+REM stop all running containers
+docker stop $(docker ps -a -q)
+
+REM run the webservice inside a container
 docker run -p 8080:9000 owin-web-service
+
+REM attach to the container's bash shell
+docker run -it -p 8080:9000 owin-web-service /bin/bash
